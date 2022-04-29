2566491ad56e26cba7abddb81488b2800f8172f1
Danilove <dansousa0(@gmail.com>
2022-04-28 22:16:34 -0300

    feat: add redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,64 +1,49 @@
-import Head from "next/head";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { CircularProgress } from "@mui/material";
+import Typist from "react-typist";
 
-import { Container } from "../styles/pages/Home";
-import { GetServerSideProps } from "next";
-import { IData } from "../interfaces";
-import Header from "../components/Header";
-import List from "../components/List";
-import Pagination from "../components/Pagination";
-import getHomeworld from "../utils/gethomeworld";
-import { Backdrop, CircularProgress } from '@mui/material';
+import catchPhrases from "../utils/catchPhrases";
 
+const Container = styled.main`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center; 
 
-interface Props {
-    data: IData;
-}
+    color: #fff;
+`;
 
-const Home: React.FC<Props> = ({ data }) => {
-    console.log(data);
-    const [backdropOpen, setBackdropOpen] = useState(false);
+const Loading = styled.h1`
+    font-size: 2rem;
+    font-weight: bold;
+    margin: 1rem 0;
+`;
+
+const Paragraph = styled.p`
+    opacity: 0.6;
+    font-size: 0.8rem;
+    font-style: italic;
+`;
+
+const Redirect = () => {
 
     useEffect(() => {
-        setBackdropOpen(false);
-    }, [data]);
-
+        window.location.href = window.location.href + "/page/1";
+    }, []);
 
     return (
         <Container>
-            <Head>
-                <title>Swapi</title>
-            </Head>
-            <Header />
-            <List data={data} />
-            <Pagination data={data} setBackdropOpen={setBackdropOpen} />
-            <Backdrop
-                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-                open={backdropOpen}
-                onClick={() => setBackdropOpen(false)}
-            >
-                <CircularProgress color="inherit" />
-            </Backdrop>
+            <CircularProgress size={65} color="warning" />
+            <Loading>Carregando...</Loading>
+            <Typist>
+                <Paragraph>{catchPhrases[Math.floor(Math.random() * catchPhrases.length)]}</Paragraph>
+            </Typist>
         </Container>
-    );
+    )
 };
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-    const { page } = ctx.query;
-
-    const res = await fetch(page ?
-        `http://localhost:3000/api/characters/?page=${page}` : 'http://localhost:3000/api/characters/');
-    const data: IData = await res.json();
-
-    for (let i = 0; i < data.results.length; i++) {
-        let homeworlds = await getHomeworld(data.results[i].homeworld);
-        data.results[i].homeworld = homeworlds.name;
-    }
-
-    return {
-        props: { data }
-    }
-}
-
-export default Home;
+export default Redirect;
 
---
+++ src/utils/catchPhrases.ts
@@ -0,0 +1,10 @@
+const catchPhrases = [
+    "'Que monte de sucata!'",
+    "'Quando o deixei, eu era só o aprendiz... agora eu sou o mestre'",
+    "'Eu sou seu pai'",
+    "'Que a Força esteja com você'",
+    "'Ajude-me Obi-Wan Kenobi'",
+    "'Aquilo não é a lua, é uma estação espacial'",
+];
+
+export default catchPhrases;
